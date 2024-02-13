84a5a82c5f8d51985f9831733a18da51a4a93f10
Miguel Buca <miguelpedrobuca@gmail.com>
2024-02-13 06:41:49 +0100

    chore: try to read on api folder

---
+++ src/app/api/route.ts
@@ -0,0 +1,7 @@
+import fs from 'fs';
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  const text = fs.readFileSync('src/app/api/docs/mvvm-with-react-native.md');
+  return NextResponse.json(text.toString())
+}
