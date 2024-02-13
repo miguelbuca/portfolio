d6991b6cbd7a27871b0d5a71f3b3e4b7e723f0ff
Miguel Buca <miguelpedrobuca@gmail.com>
2024-02-13 07:47:06 +0100

    fix: removed fs in client side

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,14 @@
-import fs from 'fs';
-import { NextResponse } from 'next/server'
+import fs from "fs";
+import { NextResponse } from "next/server";
+import type { NextApiRequest } from 'next'
 
-export async function GET() {
-  const text = fs.readFileSync('src/app/api/docs/mvvm-with-react-native.md');
-  return NextResponse.json(text.toString())
+export async function GET(
+  req: NextApiRequest
+) {
+  const slug = req.url?.split('=')[1]
+
+  if(!slug)return
+
+  const text = fs.readFileSync(`src/app/api/docs/${slug}.md`);
+  return NextResponse.json(text.toString());
 }
--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import fs from "fs";
+"use client";
+import React, { useEffect, useState } from "react";
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import ReactMarkdown from "react-markdown";
-import remarkGfm from 'remark-gfm'
+import remarkGfm from "remark-gfm";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
 import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
@@ -11,6 +11,7 @@ import php from "react-syntax-highlighter/dist/cjs/languages/prism/php";
 import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
 import markdown from "react-syntax-highlighter/dist/cjs/languages/prism/markdown";
 import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+import axios from "axios";
 
 SyntaxHighlighter.registerLanguage("tsx", tsx);
 SyntaxHighlighter.registerLanguage("ts", typescript);
@@ -27,10 +28,6 @@ type CodeProps = {
 };
 
 type ArticleProps = { params: { slug: string } };
-
-const getArticle = (slug: string) => {
-  return fs.readFileSync(`src/docs/${slug}.md`, "utf-8");
-};
 
 const Code = ({ className, children }: CodeProps) => {
   return (
@@ -49,6 +46,12 @@ const Code = ({ className, children }: CodeProps) => {
 };
 
 const Article = ({ params }: ArticleProps) => {
+  const [markdown, setMarkdown] = useState("");
+
+  useEffect(() => {
+    axios.get(`/api?file=${params.slug}`).then(({ data }) => setMarkdown(data));
+  }, [params.slug]);
+
   return (
     <div className="container pb-16 mt-24">
       <pre className="[&_p]:whitespace-normal font-sans [&_p]:font-normal [&_h1]:whitespace-normal [&_h1]:text-2xl [&_table]:table-auto [&_table]:border-collapse [&_table_*]:border [&_table_*]:p-1 [&_table_*]:border-black/[0.06] [&_table_*]:dark:border-white/[0.06] [&_a]:text-blue-500  [&_h1]:mb-3 [&_li]:text-sm [&_p]:text-sm [&_li]:opacity-60 [&_p]:opacity-60 [&_ul]:leading-3  [&_li]:whitespace-normal [&_ul]:list-none">
@@ -58,7 +61,7 @@ const Article = ({ params }: ArticleProps) => {
           }}
           remarkPlugins={[remarkGfm]}
         >
-          {getArticle(params.slug)}
+          {markdown}
         </ReactMarkdown>
       </pre>
     </div>
